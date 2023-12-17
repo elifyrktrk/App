733bfc7e6411eaed8682d0cc471a4242e45b5969
elif yürektürk <elifyrktrk@gmail.com>
2023-12-17 14:48:44 +0300

    tried listing the contents

--- App/App/HomeViewController.swift
+++ App/App/HomeViewController.swift
@@ -1,10 +1,3 @@
-//
-//  HomeViewController.swift
-//  App
-//
-//  Created by Elif Yürektürk on 1.12.2023.
-//
-
 import UIKit
 import FirebaseDatabase
 import FirebaseAuth
@@ -12,31 +5,57 @@ import FirebaseAuth
 class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var databaseRef = Database.database().reference()
+    var loggedInUser: User?
+    var loggedInUserData: [String: Any]?
 
-    
+    var contents = [AnyObject?]()
+
     @IBOutlet weak var homeTableView: UITableView!
     @IBOutlet weak var aivLoading: UIActivityIndicatorView!
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-          }
         
+        self.loggedInUser = Auth.auth().currentUser
 
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        self.databaseRef.child("user_profiles").child(self.loggedInUser!.uid).observeSingleEvent(of: .value) { (snapshot) in
+           
+            if let data = snapshot.value as? [String: Any] {
+                self.loggedInUserData = data
+                
+                if let uid = data["uid"] as? String {
+                    self.databaseRef.child("contents/\(uid)").observe(.childAdded) { (contentSnapshot) in                    self.contents.append(contentSnapshot)
+                        let indexPath = IndexPath(row: self.contents.count - 1, section: 0)
+                        self.homeTableView.insertRows(at: [indexPath], with: .automatic)
+                        
+                        self.aivLoading.stopAnimating()
+                    }
+                }
+            }
+        }
     }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    
+
+        func numberOfSections(in tableView: UITableView) -> Int {
+            return 1
+        }
+
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return self.contents.count
+        }
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell: HomeViewTableViewCell = tableView.dequeueReusableCell(withIdentifier: "HomeViewTableViewCell", for: indexPath) as! HomeViewTableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HomeViewTableViewCell", for: indexPath) as! HomeViewTableViewCell
         
-        return cell
+        let content = contents[(self.contents.count - 1) - indexPath.row] as? String
+       
+        cell.configure(profilePic: nil,name:self.loggedInUserData!["name"] as! String,handle:self.loggedInUserData!["handle"] as! String, content:content!)
+        print("TableView hücresi oluşturuldu: \(cell)")
+
+           return cell
+
+        }
     }
-    
-}
+
+
+
