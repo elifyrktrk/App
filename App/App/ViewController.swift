9779b6774de13dd282b44d472b6167db9bd977c5
elif yürektürk <elifyrktrk@gmail.com>
2023-12-23 22:59:23 +0300

    send the user to the home screen

--- App/App/ViewController.swift
+++ App/App/ViewController.swift
@@ -6,13 +6,31 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        Auth.auth().addStateDidChangeListener({ (auth, user) in
+            
+            if let currentUser = user{
+                
+                print("user is signed in")
+                
+//                send the user to homeViewController
+                
+                let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                
+                let homeViewController: UIViewController = mainStoryboard.instantiateViewController(withIdentifier: "TabBarControllerView")
+                
+//            send the user to the Home screen
+                self.present(homeViewController, animated: true, completion: nil)
+
+            }
+            
+        })
        
         
     }
